bf396ae4b8d2380bb8024f8d794771a7f3b48fa2
Daniel Baumann <daniel@debian.org>
2007-09-23 10:04:49 +0200

    Adding live-helper 1.0~a5-1.

--- functions/breakpoints.sh
+++ functions/breakpoints.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-# breakpoints.sh
+# breakpoints.sh - run with breakpoints
 # Copyright (C) 2006-2007 Daniel Baumann <daniel@debian.org>
 #
 # live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
 # This is free software, and you are welcome to redistribute it
 # under certain conditions; see COPYING for details.
+
+Breakpoint ()
+{
+	if [ "${LH_BREAKPOINTS}" = "enabled" ]
+	then
+		BREAKPOINT="${1}"
+
+		Echo_message "Waiting at ${BREAKPOINT}"
+		read
+	fi
+}
--- functions/common.sh
+++ functions/common.sh
@@ -10,4 +10,4 @@
 set -e
 
 PROGRAM="`basename ${0}`"
-VERSION="1.0~a4"
+VERSION="1.0~a5"
