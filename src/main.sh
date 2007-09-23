03c8a20c280c6888b4d274a3095013064b94e99b
Daniel Baumann <daniel@debian.org>
2007-09-23 10:04:35 +0200

    Adding live-package 0.99.13-1.

--- src/main.sh
+++ src/main.sh
@@ -28,7 +28,7 @@ set -e
 BASE=${LIVE_BASE:-"/usr/share/make-live"}
 CONFIG="/etc/make-live.conf"
 PROGRAM="`basename ${0}`"
-VERSION="0.99.12"
+VERSION="0.99.13"
 
 CODENAME_OLDSTABLE="woody"
 CODENAME_STABLE="sarge"
