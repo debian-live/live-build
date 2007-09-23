799d4bfb31c279b72088c8ee8a16c2710ab8a309
Daniel Baumann <daniel@debian.org>
2007-09-23 10:04:48 +0200

    Adding live-helper 1.0~a3-1.

--- functions/chroot.sh
+++ functions/chroot.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-# chroot.sh - chroot wrapper
+# chroot.sh - /usr/sbin/chroot wrapper script
+# Copyright (C) 2006-2007 Daniel Baumann <daniel@debian.org>
+#
+# live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see COPYING for details.
 
 set -e
 
