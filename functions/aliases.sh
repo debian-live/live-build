889489c8ce3339ca3b6cab45ec58e73cca40a51a
Daniel Baumann <daniel@debian.org>
2007-11-10 15:40:53 +0100

    Using shell redirection to truncate rather than additional commands/forks/files, thanks to Justin Pryzby <jpryzby@quoininc.com>.

---
+++ functions/aliases.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# aliases.sh - internal shell aliases
+# Copyright (C) 2006-2007 Daniel Baumann <daniel@debian.org>
+#
+# live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see COPYING for details.
+
+set -e
+
+Truncate ()
+{
+	for FILE in ${@}
+	do
+		: > ${FILE}
+	done
+}
