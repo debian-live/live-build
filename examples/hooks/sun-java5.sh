b4b0384b619efe64cbae0ca1258730d60af33afe
Daniel Baumann <daniel@debian.org>
2007-11-10 17:38:36 +0100

    Documenting preseeding for sun-java hooks, thanks to Antonio Amorim <Antonio.Amorim@sim.fc.ul.pt>.

--- examples/hooks/sun-java5.sh
+++ examples/hooks/sun-java5.sh
@@ -11,6 +11,13 @@
 # Java do not allow this, therefore we need to overwrite DEBIAN_FRONTEND for
 # this apt-get call only.
 
+# In case you do accept the license terms, you can also preseed the values
+# for a complete non-interactive build, by uncommenting the following three lines:
+
+#echo "sun-java5-bin shared/accepted-sun-dlj-v1-1 boolean true" > /root/preseed
+#debconf-set-selections < /root/preseed
+#rm -f /root/preseed
+
 DEBIAN_FRONTEND="dialog" apt-get install --yes sun-java5-bin sun-java5-demo \
 	sun-java5-doc sun-java5-fonts sun-java5-jdk sun-java5-jre \
 	sun-java5-plugin sun-java5-source
--- examples/hooks/sun-java6.sh
+++ examples/hooks/sun-java6.sh
@@ -11,6 +11,13 @@
 # Java do not allow this, therefore we need to overwrite DEBIAN_FRONTEND for
 # this apt-get call only.
 
+# In case you do accept the license terms, you can also preseed the values
+# for a complete non-interactive build, by uncommenting the following three lines:
+
+#echo "sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true" > /root/preseed
+#debconf-set-selections < /root/preseed
+#rm -f /root/preseed
+
 DEBIAN_FRONTEND="dialog" apt-get install --yes sun-java6-bin sun-java6-demo \
 	sun-java6-doc sun-java6-fonts sun-java6-jdk sun-java6-jre \
 	sun-java6-plugin sun-java6-source
