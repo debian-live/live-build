042354c7df657a4d8dc3aab251a5920fbb152cbf
Daniel Baumann <daniel@debian.org>
2007-12-22 09:45:48 +0100

    Switching to GPL3+.

--- functions/version.sh
+++ functions/version.sh
@@ -16,9 +16,9 @@ Version ()
 	echo
 	echo "Copyright (C) 2006-2007 Daniel Baumann <daniel@debian.org>"
 	echo
-	echo "This program is free software; you can redistribute it and/or modify"
+	echo "This program is free software: you can redistribute it and/or modify"
 	echo "it under the terms of the GNU General Public License as published by"
-	echo "the Free Software Foundation; either version 2 of the License, or"
+	echo "the Free Software Foundation, either version 3 of the License, or"
 	echo "(at your option) any later version."
 	echo
 	echo "This program is distributed in the hope that it will be useful,"
@@ -27,11 +27,10 @@ Version ()
 	echo "GNU General Public License for more details."
 	echo
 	echo "You should have received a copy of the GNU General Public License"
-	echo "along with this program; if not, write to the Free Software"
-	echo "Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA"
+	echo "along with this program. If not, see <http://www.gnu.org/licenses/>."
 	echo
 	echo "On Debian systems, the complete text of the GNU General Public License"
-	echo "can be found in /usr/share/common-licenses/GPL-2 file."
+	echo "can be found in /usr/share/common-licenses/GPL-3 file."
 	echo
 	echo "Homepage: <http://debian-live.alioth.debian.org/>"
 
