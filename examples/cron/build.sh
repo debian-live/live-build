5d970f543638ba6218426e0a94f11e9a78d97c2c
Daniel Baumann <daniel@debian.org>
2008-04-17 12:13:49 +0200

    Adding example script to build a row of images.

---
+++ examples/cron/build.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -x
+
+set -e
+
+DATE="$(date +%Y%m%d)"
+
+for DISTRIBUTION in etch lenny sid
+do
+	rm -rf cache/stages*
+
+	for FLAVOUR in standard gnome-desktop kde-desktop xfce-desktop
+	do
+		mkdir -p config
+
+		if [ -e .stage ]
+		then
+			lh clean
+		fi
+
+		rm -rf config
+		rm -rf cache/packages*
+		rm -rf cache/stages_rootfs
+
+		lh config -d ${DISTRIBUTION} -p ${FLAVOUR} --cache-stages "bootstrap rootfs" --source enabled --mirror-bootstrap http://mirror/ftp.debian.org/debian/ --mirror-chroot http://mirror/ftp.debian.org/debian/ --mirror-chroot-security http://mirror/ftp.debian.org/debian-security/
+
+		if [ "${DISTRIBUTION}" = "sid" ]
+		then
+			echo 'deb http://live.debian.net/debian-snapshots/ ./' > config/chroot_sources/live-snapshots.chroot
+			echo 'deb http://live.debian.net/debian-snapshots/ ./' > config/chroot_sources/live-snapshots.boot
+
+			wget http://ftp-master.debian-unofficial.org/other/openpgp/archive-key-2008.asc -O config/chroot_sources/live-snapshots.chroot.gpg
+			wget http://ftp-master.debian-unofficial.org/other/openpgp/archive-key-2008.asc -O config/chroot_sources/live-snapshots.binary.gpg
+
+		fi
+
+		lh build | tee debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.iso.log
+
+		mv binary.iso debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.iso
+		mv source.tar.gz debian-live-${DISTRIBUTION}-source-${FLAVOUR}.tar.gz
+		mv binary.packages debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.iso.packages
+		mv binary.list debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.iso.list
+
+		lh clean --binary
+		lh config -b usb-hdd
+		lh binary | tee debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.img.log
+
+		mv binary.img debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.img
+		mv binary.packages debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.img.packages
+		mv binary.list debian-live-${DISTRIBUTION}-i386-${FLAVOUR}.img.list
+	done
+done
