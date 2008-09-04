b785db39a2cfc8beb4b5274ff9b99407fc4707a0
Daniel Baumann <daniel@debian.org>
2008-09-04 10:05:50 +0200

    Adding color functions for future usage.

---
+++ functions/color.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# color.sh - define color values
+# Copyright (C) 2006-2008 Daniel Baumann <daniel@debian.org>
+#
+# live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see COPYING for details.
+
+set -e
+
+NO_COLOR="[0m"
+
+UNDERSCORE="[4m"
+BLINK="[5m"
+
+BLACK="[0;30m"
+DARK_GRAY="[1;30m"
+
+RED="[0;31m"
+LIGHT_RED="[1;31m"
+
+GREEN="[0;32m"
+LIGHT_GREEN="[1;32m"
+
+BROWN="[0;33m"
+YELLOW="[1;33m"
+
+BLUE="[0;34m"
+LIGHT_BLUE="[1;34m"
+
+PURPLE="[0;35m"
+LIGHT_PURPLE="[1;35m"
+
+CYAN="[0;36m"
+LIGHT_CYAN="[1;36m"
+
+GRAY="[0;37m"
+WHITE="[1;37m"
