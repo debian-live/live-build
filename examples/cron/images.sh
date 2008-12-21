a1d929c9ad587ad5321c2a31cc6297d555dea39a
Daniel Baumann <daniel@debian.org>
2008-12-21 09:56:06 +0100

    Updating location of archive key in in image autobuild script.

--- examples/cron/images.sh
+++ examples/cron/images.sh
@@ -41,8 +41,8 @@ do
 			echo 'deb http://live.debian.net/debian/ ./' > config/chroot_sources/live-snapshots.chroot
 			echo 'deb http://live.debian.net/debian/ ./' > config/chroot_sources/live-snapshots.boot
 
-			wget http://ftp-master.debian-unofficial.org/other/openpgp/archive-key-2008.asc -O config/chroot_sources/live-snapshots.chroot.gpg
-			wget http://ftp-master.debian-unofficial.org/other/openpgp/archive-key-2008.asc -O config/chroot_sources/live-snapshots.binary.gpg
+			wget http://live.debian.net/debian/archive.gpg -O config/chroot_sources/live-snapshots.chroot.gpg
+			wget http://live.debian.net/debian/archive.gpg -O config/chroot_sources/live-snapshots.binary.gpg
 
 		fi
 
