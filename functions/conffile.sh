989f26d87b5ef36e1f1fd419a9ff2996b3f4b360
Daniel Baumann <daniel@debian.org>
2009-01-26 17:39:27 +0100

    Updating year in copyright notices for 2009.

--- functions/conffile.sh
+++ functions/conffile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # conffile.sh - handle configuration files
-# Copyright (C) 2006-2008 Daniel Baumann <daniel@debian.org>
+# Copyright (C) 2006-2009 Daniel Baumann <daniel@debian.org>
 #
 # live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
 # This is free software, and you are welcome to redistribute it
