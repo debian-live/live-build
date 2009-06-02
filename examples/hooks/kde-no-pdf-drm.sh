6bb915f3b4e57b3a8a9612a7dcd4ce03af55828e
Daniel Baumann <daniel@debian.org>
2009-06-02 17:10:36 +0200

    Updating kpdf-nodrm hook to also work with KDE4.

---
+++ examples/hooks/kde-no-pdf-drm.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# This is a hook for live-helper(7) to configure KDEs PDF viewer to ignore
+# manipulation restrcition on "DRM protect" PDF documents.
+#
+# To enable it, copy or symlink this hook into your config/chroot_local-hooks
+# directory.
+
+if [ -d /usr/share/kde4/config ]
+then
+	# KDE4 (squeeze/sid)
+
+cat > /usr/share/kde4/config/okularpartrc << EOF
+[General]
+ObeyDRM=false
+EOF
+
+elif [ -d /usr/share/config ]
+then
+	# KDE3 (etch/lenny)
+
+cat > /usr/share/config/kpdfpartrc << EOF
+[General]
+ObeyDRM=false
+EOF
+
+fi
