a87d976add27e00de34b6f1ed19021ed012b3620
Daniel Baumann <daniel@debian.org>
2009-10-21 13:12:48 +0200

    Allow to overload global functions with local functions stored in the config tree at scripts/functions.
    live-helper 1.0.6 will be the last release of live-helper 1.x, which will
    produce lenny 5.0.3 release images. However, when 5.0.4 is due, we'll need
    to produce new images with slightly modified functions/release.sh files.
    In order to acchieve that, we'll allow to have such a file overload the
    functions shipped in live-helper.
    
    This way, we can acchieve that:
    
      * live-helper itself doesn't need to be updated through stable updates
        anymore.
      * we still can do the required changes for a new point release.
      * we even ship the changes in the source tarballs automatically.

--- functions.sh
+++ functions.sh
@@ -2,7 +2,17 @@
 
 LH_BASE="${LH_BASE:-/usr/share/live-helper}"
 
+# Source global functions
 for FUNCTION in "${LH_BASE}"/functions/*.sh
 do
 	. "${FUNCTION}"
 done
+
+# Source local functions
+if [ -d scripts/functions ]
+then
+	for FUNCTION in scripts/functions/*.sh
+	do
+		. "${FUNCTION}"
+	done
+fi
