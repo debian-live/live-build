24b2893bda68e72ffae5b3b0c86d1b2295d6b5f6
Daniel Baumann <daniel@debian.org>
2009-11-15 19:11:46 +0100

    Adding upstream version 1.0~a4.

--- functions/chroot.sh
+++ functions/chroot.sh
@@ -14,5 +14,6 @@ Chroot ()
 	COMMANDS="${1}"
 
 	# Executing commands in chroot
+	Echo_debug "Executing: ${COMMANDS}"
 	chroot chroot /usr/bin/env -i HOME="/root" PATH="/usr/sbin:/usr/bin:/sbin:/bin" TERM="${TERM}" ftp_proxy="${LH_APT_FTPPROXY}" http_proxy="${LH_APT_HTTPPPROXY}" DEBIAN_FRONTEND="${LH_DEBCONF_FRONTEND}" DEBIAN_PRIORITY="${LH_DEBCONF_PRIORITY}" ${COMMANDS}
 }
