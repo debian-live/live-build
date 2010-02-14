5884164e7921a62e0c07de45f6abb3501e8683be
Daniel Baumann <daniel@debian.org>
2010-02-14 15:43:47 +0100

    Calling /usr/sbin/chroot with full path in Chroot(); should fix path problems when using live-helper from a local source tree.

--- functions/chroot.sh
+++ functions/chroot.sh
@@ -17,10 +17,10 @@ Chroot ()
 
 	if [ "${LH_USE_FAKEROOT}" != "true" ]
 	then
-		${LH_ROOT_COMMAND} chroot "${CHROOT}" /usr/bin/env -i HOME="/root" PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" TERM="${TERM}" ftp_proxy="${LH_APT_FTP_PROXY}" http_proxy="${LH_APT_HTTP_PROXY}" DEBIAN_FRONTEND="${LH_DEBCONF_FRONTEND}" DEBIAN_PRIORITY="${LH_DEBCONF_PRIORITY}" DEBCONF_NOWARNINGS="${LH_DEBCONF_NOWARNINGS}" XORG_CONFIG="custom" ${COMMANDS}
+		${LH_ROOT_COMMAND} /usr/sbin/chroot "${CHROOT}" /usr/bin/env -i HOME="/root" PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin" TERM="${TERM}" ftp_proxy="${LH_APT_FTP_PROXY}" http_proxy="${LH_APT_HTTP_PROXY}" DEBIAN_FRONTEND="${LH_DEBCONF_FRONTEND}" DEBIAN_PRIORITY="${LH_DEBCONF_PRIORITY}" DEBCONF_NOWARNINGS="${LH_DEBCONF_NOWARNINGS}" XORG_CONFIG="custom" ${COMMANDS}
 	else
 		# Building with fakeroot/fakechroot
-		${LH_ROOT_COMMAND} chroot "${CHROOT}" ${COMMANDS}
+		${LH_ROOT_COMMAND} /usr/sbin/chroot "${CHROOT}" ${COMMANDS}
 	fi
 
 	return "${?}"
