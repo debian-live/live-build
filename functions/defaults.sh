13f67e41576d2ce2abb46b6e420fecd5e4fdfb66
Daniel Baumann <daniel@debian.org>
2010-02-17 12:18:34 +0100

    Don't append and re-append try-usb parameter to bootappend-install options over and over again.

--- functions/defaults.sh
+++ functions/defaults.sh
@@ -782,7 +782,10 @@ Set_defaults ()
 	if [ "${LH_BINARY_IMAGES}" = "usb-hdd" ]
 	then
 		# Try USB block devices for install media
-		LH_BOOTAPPEND_INSTALL="cdrom-detect/try-usb=true ${LH_BOOTAPPEND_INSTALL}"
+		if ! echo "${LH_BOOTAPPEND_INSTALL}" | grep -q try-usb
+		then
+			LH_BOOTAPPEND_INSTALL="cdrom-detect/try-usb=true ${LH_BOOTAPPEND_INSTALL}"
+		fi
 	fi
 
 	if [ -n ${_LH_BOOTAPPEND_PRESEED} ]
