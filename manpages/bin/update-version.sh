28a565b64184dfe8fbe91ae076b3ef8fcab156ca
Daniel Baumann <daniel@debian.org>
2010-06-06 11:23:19 +0200

    Updating update-version script.

--- manpages/bin/update-version.sh
+++ manpages/bin/update-version.sh
@@ -4,11 +4,15 @@ set -e
 
 . ../functions/common.sh
 
+DATE="$(LC_ALL=C date +%Y\\\\-%m\\\\-%d)"
+PROGRAM="LIVE\\\-HELPER"
+VERSION="${CONFIG_VERSION}"
+
 echo "Updating version headers..."
 
 for MANPAGE in en/*
 do
 	SECTION="$(basename ${MANPAGE} | awk -F. '{ print $2 }')"
 
-	sed -i -e "s|^.TH.*$|.TH LIVE\\\-HELPER ${SECTION} $(date +%Y\\\\-%m\\\\-%d) ${CONFIG_VERSION} \"Debian Live Project\"|" ${MANPAGE}
+	sed -i -e "s|^.TH.*$|.TH ${PROGRAM} ${SECTION} ${DATE} ${VERSION} \"Debian Live Project\"|" ${MANPAGE}
 done
