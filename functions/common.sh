9db8431782f9597fe565c5baee09dbe0d9bb6794
Daniel Baumann <daniel@debian.org>
2010-06-18 11:39:39 +0200

    Adding upstream version 2.0~a15.

--- functions/common.sh
+++ functions/common.sh
@@ -9,7 +9,7 @@
 
 PROGRAM="$(basename ${0})"
 PACKAGE="live-helper"
-VERSION="2.0~a14-1"
+VERSION="2.0~a15-1"
 CONFIG_VERSION="$(echo ${VERSION} | awk -F- '{ print $1 }')"
 
 PATH="${PWD}/auto/helpers:${PATH}"
