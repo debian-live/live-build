4d22ca948ad3def3f98fd89d32bf2ba8209460fd
Daniel Baumann <daniel@debian.org>
2010-09-02 13:12:37 +0200

    Updating copyright headers for live-build.

--- functions/common.sh
+++ functions/common.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# common.sh - common things for all live-helpers
-# Copyright (C) 2006-2010 Daniel Baumann <daniel@debian.org>
-#
-# live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
-# This is free software, and you are welcome to redistribute it
-# under certain conditions; see COPYING for details.
+## live-build(7) - System Build Scripts
+## Copyright (C) 2006-2010 Daniel Baumann <daniel@debian.org>
+##
+## live-build comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+## This is free software, and you are welcome to redistribute it
+## under certain conditions; see COPYING for details.
 
-PROGRAM="$(basename ${0})"
+
 PACKAGE="live-helper"
 VERSION="2.0~a23-1"
 CONFIG_VERSION="$(echo ${VERSION} | awk -F- '{ print $1 }')"
--- functions/l10n.sh
+++ functions/l10n.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# l10n.sh - load the needed localization things for all lh messages
-# Copyright (C) 2006-2010 Daniel Baumann <daniel@debian.org>
-#
-# live-helper comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
-# This is free software, and you are welcome to redistribute it
-# under certain conditions; see COPYING for details.
+## live-build(7) - System Build Scripts
+## Copyright (C) 2006-2010 Daniel Baumann <daniel@debian.org>
+##
+## live-build comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+## This is free software, and you are welcome to redistribute it
+## under certain conditions; see COPYING for details.
+
 
 if [ -x "$(which gettext.sh 2>/dev/null)" ] && Find_files /usr/share/locale/*/LC_MESSAGES/${PACKAGE}.mo
 then
