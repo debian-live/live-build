7e8240cd5e063895ee6e16745e0ea4b22658c6e5
Daniel Baumann <daniel@debian.org>
2012-12-17 13:51:44 +0100

    Removing kubuntu mode, it's not different anymore to standard ubuntu mode.

--- functions/defaults.sh
+++ functions/defaults.sh
@@ -21,7 +21,7 @@ Set_defaults ()
 	# Setting system type
 	LB_SYSTEM="${LB_SYSTEM:-live}"
 
-	# Setting mode (currently: debian, emdebian, progress-linux, ubuntu and kubuntu)
+	# Setting mode (currently: debian, emdebian, progress-linux, and ubuntu)
 	if [ -x /usr/bin/lsb_release ]
 	then
 		_DISTRIBUTOR="$(lsb_release -is | tr [A-Z] [a-z])"
@@ -54,7 +54,7 @@ Set_defaults ()
 			LB_DERIVATIVE="true"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			LB_DISTRIBUTION="${LB_DISTRIBUTION:-precise}"
 			LB_DERIVATIVE="false"
 			;;
@@ -176,7 +176,7 @@ Set_defaults ()
 	case "${LB_SYSTEM}" in
 		live)
 			case "${LB_MODE}" in
-				ubuntu|kubuntu)
+				ubuntu)
 					LB_INITRAMFS="${LB_INITRAMFS:-casper}"
 					;;
 
@@ -195,7 +195,7 @@ Set_defaults ()
 
 	# Setting initsystem
 	case "${LB_MODE}" in
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_INITRAMFS}" in
 				live-boot)
 					LB_INITSYSTEM="${LB_INITSYSTEM:-upstart}"
@@ -365,7 +365,7 @@ Set_defaults ()
 			LB_MIRROR_BOOTSTRAP="${LB_MIRROR_BOOTSTRAP:-http://cdn.archive.progress-linux.org/progress/}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_ARCHITECTURES}" in
 				amd64|i386)
 					LB_MIRROR_BOOTSTRAP="${LB_MIRROR_BOOTSTRAP:-http://archive.ubuntu.com/ubuntu/}"
@@ -400,7 +400,7 @@ Set_defaults ()
 			LB_MIRROR_CHROOT_SECURITY="${LB_MIRROR_CHROOT_SECURITY:-${LB_MIRROR_CHROOT}}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_ARCHITECTURES}" in
 				amd64|i386)
 					LB_MIRROR_CHROOT_SECURITY="${LB_MIRROR_CHROOT_SECURITY:-http://security.ubuntu.com/ubuntu/}"
@@ -422,7 +422,7 @@ Set_defaults ()
 			LB_MIRROR_CHROOT_UPDATES="${LB_MIRROR_CHROOT_UPDATES:-${LB_MIRROR_CHROOT}}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_ARCHITECTURES}" in
 				amd64|i386)
 					LB_MIRROR_CHROOT_UPDATES="${LB_MIRROR_CHROOT_UPDATES:-http://archive.ubuntu.com/ubuntu/}"
@@ -476,7 +476,7 @@ Set_defaults ()
 			LB_PARENT_MIRROR_BINARY="${LB_PARENT_MIRROR_BINARY:-${LB_MIRROR_BINARY}}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_ARCHITECTURES}" in
 				amd64|i386)
 					LB_MIRROR_BINARY="${LB_MIRROR_BINARY:-http://archive.ubuntu.com/ubuntu/}"
@@ -508,7 +508,7 @@ Set_defaults ()
 			LB_MIRROR_BINARY_SECURITY="${LB_MIRROR_BINARY_SECURITY:-${LB_MIRROR_CHROOT}}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_ARCHITECTURES}" in
 				amd64|i386)
 					LB_MIRROR_BINARY_SECURITY="${LB_MIRROR_BINARY_SECURITY:-http://security.ubuntu.com/ubuntu/}"
@@ -535,7 +535,7 @@ Set_defaults ()
 			LB_MIRROR_BINARY_UPDATES="${LB_MIRROR_BINARY_UPDATES:-${LB_MIRROR_BINARY}}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			case "${LB_ARCHITECTURES}" in
 				amd64|i386)
 					LB_MIRROR_BINARY_UPDATES="${LB_MIRROR_BINARY_UPDATES:-http://archive.ubuntu.com/ubuntu/}"
@@ -590,7 +590,7 @@ Set_defaults ()
 			LB_PARENT_ARCHIVE_AREAS="${LB_PARENT_ARCHIVE_AREAS:-${LB_ARCHIVE_AREAS}}"
 			;;
 
-		ubuntu|kubuntu)
+		ubuntu)
 			LB_ARCHIVE_AREAS="${LB_ARCHIVE_AREAS:-main restricted}"
 			LB_PARENT_ARCHIVE_AREAS="${LB_PARENT_ARCHIVE_AREAS:-${LB_ARCHIVE_AREAS}}"
 			;;
@@ -636,7 +636,7 @@ Set_defaults ()
 
 	# Setting keyring packages
 	case "${LB_MODE}" in
-		ubuntu|kubuntu)
+		ubuntu)
 			LB_KEYRING_PACKAGES="${LB_KEYRING_PACKAGES:-ubuntu-keyring}"
 			;;
 
@@ -649,7 +649,7 @@ Set_defaults ()
 	case "${LB_ARCHITECTURES}" in
 		armel)
 			case "${LB_MODE}" in
-                                ubuntu|kubuntu)
+                                ubuntu)
 					LB_LINUX_FLAVOURS="${LB_LINUX_FLAVOURS:-omap}"
 					;;
 				*)
@@ -668,7 +668,7 @@ Set_defaults ()
 
 		amd64)
 			case "${LB_MODE}" in
-				ubuntu|kubuntu)
+				ubuntu)
 					LB_LINUX_FLAVOURS="${LB_LINUX_FLAVOURS:-generic}"
 					;;
 
@@ -692,7 +692,7 @@ Set_defaults ()
 					esac
 					;;
 
-				ubuntu|kubuntu)
+				ubuntu)
 					case "${LB_DISTRIBUTION}" in
 						precise)
 							LB_LINUX_FLAVOURS="${LB_LINUX_FLAVOURS:-generic-pae}"
@@ -738,7 +738,7 @@ Set_defaults ()
 					exit 1
 					;;
 
-				ubuntu|kubuntu)
+				ubuntu)
 					LB_LINUX_FLAVOURS="${LB_LINUX_FLAVOURS:-powerpc-smp powerpc64-smp e500 powerpc-e500mc}"
 					;;
 
@@ -750,7 +750,7 @@ Set_defaults ()
 
 		s390)
 			case "${LB_MODE}" in
-				progress-linux|ubuntu|kubuntu)
+				progress-linux|ubuntu)
 					Echo_error "Architecture ${LB_ARCHITECTURES} not supported in the ${LB_MODE} mode."
 					exit 1
 					;;
@@ -782,7 +782,7 @@ Set_defaults ()
 
 	# Set linux packages
 	case "${LB_MODE}" in
-		ubuntu|kubuntu)
+		ubuntu)
 			LB_LINUX_PACKAGES="${LB_LINUX_PACKAGES:-linux}"
 			;;
 
@@ -953,7 +953,7 @@ Set_defaults ()
 
 			hdd*)
 				case "${LB_MODE}" in
-					ubuntu|kubuntu)
+					ubuntu)
 						if [ "${LB_DEBIAN_INSTALLER}" = "live" ]
 						then
 							_LB_BOOTAPPEND_PRESEED="file=/cdrom/install/${LB_DEBIAN_INSTALLER_PRESEEDFILE}"
@@ -1009,10 +1009,6 @@ Set_defaults ()
 		ubuntu)
 			LB_ISO_APPLICATION="${LB_ISO_APPLICATION:-Ubuntu Live}"
 			;;
-
-		kubuntu)
-			LB_ISO_APPLICATION="${LB_ISO_APPLICATION:-Kubuntu Live}"
-			;;
 	esac
 
 	# Set iso preparer
@@ -1046,10 +1042,6 @@ Set_defaults ()
 		ubuntu)
 			LB_HDD_LABEL="${LB_HDD_LABEL:-UBUNTU}"
 			;;
-
-		kubuntu)
-			LB_HDD_LABEL="${LB_HDD_LABEL:-KUBUNTU}"
-			;;
 	esac
 
 	# Setting hdd size
@@ -1072,10 +1064,6 @@ Set_defaults ()
 		ubuntu)
 			LB_ISO_VOLUME="${LB_ISO_VOLUME:-Ubuntu ${LB_DISTRIBUTION} \$(date +%Y%m%d-%H:%M)}"
 			;;
-
-		kubuntu)
-			LB_ISO_VOLUME="${LB_ISO_VOLUME:-Ubuntu ${LB_DISTRIBUTION} \$(date +%Y%m%d-%H:%M)}"
-			;;
 	esac
 
 	# Setting memtest option
@@ -1083,7 +1071,7 @@ Set_defaults ()
 
 	# Setting loadlin option
 	case "${LB_MODE}" in
-		progress-linux|ubuntu|kubuntu)
+		progress-linux|ubuntu)
 
 			;;
 
@@ -1107,7 +1095,7 @@ Set_defaults ()
 
 	# Setting win32-loader option
 	case "${LB_MODE}" in
-		progress-linux|ubuntu|kubuntu)
+		progress-linux|ubuntu)
 
 			;;
 
