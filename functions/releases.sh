222c50116f0d3492e40db04c203b42479d55de51
Daniel Baumann <mail@daniel-baumann.ch>
2013-06-19 08:03:40 +0200

    Adding upstream version 4.0~a20.

--- functions/releases.sh
+++ functions/releases.sh
@@ -9,18 +9,16 @@
 
 
 # Debian releases
-RELEASE_wheezy="7.0.0"
+RELEASE_wheezy="7"
 RELEASE_jessie="8"
 RELEASE_sid="unstable"
 
 # Progress Linux releases
-RELEASE_baureo="2.0"
-RELEASE_charon="unstable"
+RELEASE_baureo="2"
+RELEASE_charon="3"
 
 # Ubuntu releases
-RELEASE_hardy="8.04"		# LTS: 2013-04-01
 RELEASE_lucid="10.04"		# LTS: 2015-04-01
-RELEASE_oneiric="11.10"		# 2013-04-01
 RELEASE_precise="12.04"		# LTS: 2017-04-01
 RELEASE_quantal="12.10"		# 2014-04-01
 RELEASE_raring="13.04"		# 2014-10-01
